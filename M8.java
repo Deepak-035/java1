94a89da5988596cc9c54295509ecb9bb988add4f
Deepak-035 <deepak.23cs035@sode-edu.in>
2024-11-12 14:20:06 +0530

    Create M8.java

---
+++ M8.java
@@ -0,0 +1,22 @@
+package pkg1;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class M8 {
+public static void main(String[] args) {
+    try {
+        File f1 = new File("src/namess.txt");
+        Scanner sc1 = new Scanner(f1);
+        Tools1 t1 = new Tools1();
+        while (sc1.hasNextLine()) {
+            String name = sc1.nextLine();
+            System.out.println(t1.reverse(name));
+        }
+        sc1.close();
+    } catch (FileNotFoundException e) {
+        System.out.println("The file 'namess.txt' was not found.");
+        e.printStackTrace();
+    }
+}
+}
